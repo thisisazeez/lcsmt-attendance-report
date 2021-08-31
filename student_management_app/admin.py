1b1f85c0d6b060a8f31fe1a850eed2c81cd4943d
thisisazeez <imfostersyndrome@gmail.com>
2021-08-31 19:55:01 +0100

    initial

---
+++ student_management_app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, NotificationStudent, NotificationStaffs
+
+# Register your models here.
+class UserModel(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, UserModel)
+
+admin.site.register(AdminHOD)
+admin.site.register(Staffs)
+admin.site.register(Courses)
+admin.site.register(Subjects)
+admin.site.register(Students)
+admin.site.register(Attendance)
+admin.site.register(AttendanceReport)
+admin.site.register(NotificationStudent)
+admin.site.register(NotificationStaffs)
