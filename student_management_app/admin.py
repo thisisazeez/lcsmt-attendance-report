c85e2ff2e40b6414c25455cd44dcf46872c69dd5
luminousbinary <boolwrld@outlook.com>
2021-09-06 15:28:54 +0100

    fixes

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-<<<<<<< HEAD
-from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport #NotificationStudent, NotificationStaffs
-=======
-from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, NotificationStudent, NotificationStaffs, Departments
->>>>>>> bf60ba236680392d8e2fc6997476aa5f15209cf9
+
+from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, Departments
 
 # Register your models here.
 class UserModel(UserAdmin):
@@ -20,11 +17,8 @@ admin.site.register(Subjects)
 admin.site.register(Students)
 admin.site.register(Attendance)
 admin.site.register(AttendanceReport)
-<<<<<<< HEAD
 # admin.site.register(NotificationStudent)
 # admin.site.register(NotificationStaffs)
-=======
-admin.site.register(NotificationStudent)
-admin.site.register(NotificationStaffs)
 admin.site.register(Departments)
->>>>>>> bf60ba236680392d8e2fc6997476aa5f15209cf9
+
+
