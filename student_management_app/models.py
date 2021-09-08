262e4fc8187c3811a28cd2a84280c0bfb2f80130
luminousbinary <boolwrld@outlook.com>
2021-09-08 17:10:05 +0100

    ready for revert

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
@@ -16,6 +17,7 @@ class SessionYearModel(models.Model):
 
 # Overriding the Default Django Auth User and adding One More Field (user_type)
 class CustomUser(AbstractUser):
+    apple= "apple"
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
 
