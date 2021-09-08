a61918c4569266379da2920861c020d1f9ce6b53
luminousbinary <boolwrld@outlook.com>
2021-09-08 18:30:16 +0100

    Fixed

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -54,6 +54,12 @@ class Departments(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
 
+class Semesters(models.Model):
+    id = models.AutoField(primary_key=True)
+    semester_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
 
 class Courses(models.Model):
     id = models.AutoField(primary_key=True)
@@ -85,8 +91,9 @@ class AssignLecturer(models.Model):
     assign_course= models.ForeignKey(Courses, on_delete=models.CASCADE, null=True, blank=True) #need to give default course
     assign_staff = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     assign_department = models.ForeignKey(Departments, on_delete=models.CASCADE, null=True, blank=True)
+    assign_intake = models.ForeignKey(Intakes, on_delete=models.CASCADE, null=True, blank=True)
     assign_subject = models.ForeignKey(Subjects, on_delete=models.CASCADE, null=True, blank=True)
-    # assign_semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    assign_semester = models.ForeignKey(Semesters, on_delete=models.CASCADE, null=True, blank=True)
     objects = models.Manager()
 # =======
 # >>>>>>> parent of df04793 (sending some incomplete work)
